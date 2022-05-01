705eeada47df5ce49f3b450b53c2d13b1290dcbe
Pebso <pedroemiliobatista1996@outlook.com>
2022-05-01 10:56:46 -0300

    exercicio

---
+++ Exercicio 2 C#.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bom dia!");
+
+string frase = Console.ReadLine();
+
+int n1 = int.Parse (Console.ReadLine());
+char ch = char.Parse (Console.ReadLine());
+double n2 = double.Parse (Console.ReadLine());
+
+string s = Console.ReadLine();
+
+string[] vet = s.Split(' ');
+
+string nome = vet[0];
+char sexo = char.Parse (vet[1]);
+int idade = int.Parse (vet[2]);
+double altura = double.Parse (vet[3]);
+
+double valor1 = 0;
+double valor2 = 0;
+
+
+Console.WriteLine("Você digitou");
+Console.WriteLine(frase);
+Console.WriteLine(n1);
+Console.WriteLine(ch);
+Console.WriteLine(n2);
+Console.WriteLine(nome);
+Console.WriteLine(sexo);
+Console.WriteLine(idade);
+Console.WriteLine(altura);
